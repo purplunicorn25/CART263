9888b4f8588b3f7ca8796adbbc5eebac4528f6f7
purplunicorn25 <40446921+purplunicorn25@users.noreply.github.com>
2020-01-21 23:12:59 -0500

    A1: added remove function to leave a hole in the pixels place

--- Assignments/Assignment_1_Pixel_Painter_Pro/js/script.js
+++ Assignments/Assignment_1_Pixel_Painter_Pro/js/script.js
@@ -5,7 +5,7 @@
 Pixel Painter Pro
 Anne Boutet
 &
-Pippin Barr (comments) :) 
+Pippin Barr (comments) :)
 
 A small DOM-based program for "painting" on div-based pixels.
 
@@ -14,26 +14,28 @@ A small DOM-based program for "painting" on div-based pixels.
 // Constants
 const NUM_PIXELS = 1000;
 const PIXEL_REVERT_DELAY = 1000;
-const DEFAULT_COLOR = 'yellow';
-const PAINT_COLOR = 'white';
+const DEFAULT_COLOR = "yellow";
+const PAINT_COLOR = "white";
 
 // Set up our starting function for when the page loads
-window.addEventListener('load', setup);
+window.addEventListener("load", setup);
 
 // setup
 //
 // Adds DIVs to the page along with event listeners that will allow
 // then to change color on mouseover.
 function setup() {
-    // A loop that runs once per pixel we need
-    for (let i = 0; i < 1000; i++) {
+  // A loop that runs once per pixel we need
+  for (let i = 0; i < 1000; i++) {
     // Create a DIV and store it in a variable
-    let pixel = document.createElement('div');
+    let pixel = document.createElement("div");
     // Add the 'pixel' class to the new element
-    pixel.setAttribute('class', 'pixel');
+    pixel.setAttribute("class", "pixel");
     // Add a mouseover handler to the new element
-      document.addEventListener('mouseover', paint);
-     // Add the element to the body of the page
+    document.addEventListener("mouseover", paint);
+    // Add a click handler to the new element
+    document.addEventListener("click", remove);
+    // Add the element to the body of the page
     document.body.appendChild(pixel);
   }
 }
@@ -41,7 +43,7 @@ function setup() {
 // paint
 //
 // Called by the mouseover event handler on each pixel. Changes
-// the pixel's color and sets a timer for it to revert
+// the pixel's color and sets a timer for it to revert.
 function paint(e) {
   // e.target contains the specific element moused over so let's
   // save that into a variable for clarity.
@@ -56,8 +58,20 @@ function paint(e) {
   // When we pass additional parameters (like 'pixel' below) they
   // are passed to the callback function (resetPixel)
   setTimeout(resetPixel, 1000, pixel);
-
 }
+
+// remove
+//
+// Called by the click event handler on each pixel. Removes
+// the opacity of the pixel to make it look like a hole.
+function remove(e) {
+  // e.target contains the specific element moused over so let's
+  // save that into a variable for clarity.
+  let pixel = e.target;
+  // Change the opacity value to 0
+  pixel.style.opacity = "0";
+}
+
 
 // resetPixel
 //
