e04b967876672ca26f4631b6a67939206b7aafa7
purplunicorn25 <40446921+purplunicorn25@users.noreply.github.com>
2020-01-23 17:33:13 -0500

    A2: Added global values and formatting

--- Assignments/Assignment_2_Raving_Redactionist_Redux/js/script.js
+++ Assignments/Assignment_2_Raving_Redactionist_Redux/js/script.js
@@ -5,28 +5,51 @@
 Activity 3
 Anne Boutet
 
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 This is a template. Fill in the title, author, and this description
 to match your project! Write JavaScript to do amazing things below!
 
 *********************************************************************/
+
+// Constants
 const REVEAL_PROBABILITY = 0.1;
 const INTERVAL_DURATION = 300;
 
-let $spans;
+// Text component that does not want to stay hidden
+let $sensitiveContent;
+// Text component that are hidden and waiting to be found
+let $secretFound;
+let $secretTotal;
 
+// Call setup when the page is loaded
 $(document).ready(setup);
 
+// setup
+//
+//
 function setup() {
+  // Define all the spans to be refered as $sensitiveContent
+  $sensitiveContent = $(".redacted");
+  // Set an interval for the $sensitiveContent to go from visible to hidden
   setInterval(update, INTERVAL_DURATION);
-  let $spans = $("span");
-  $spans.on("click", spanClicked);
+  // Check if the element is clicked by the user
+  $sensitiveContent.on("click", sensitiveContentClicked);
 }
 
+// update
+//
+//
 function update() {
-  $("span").each(updateSpan);
+  //
+  $sensitiveContent.each(updateSensitiveContent);
 }
 
-function updateSpan() {
+// updateSensitiveContent
+//
+//
+function updateSensitiveContent() {
+  //
+  //
   let randomNumber = Math.random();
   if (randomNumber < REVEAL_PROBABILITY) {
     $(this).removeClass("redacted");
@@ -34,7 +57,11 @@ function updateSpan() {
   }
 }
 
-function spanClicked() {
+// sensitiveContentClicked
+//
+//
+function sensitiveContentClicked() {
+  //
   $(this).addClass("redacted");
   $(this).removeClass("revealed");
 }
