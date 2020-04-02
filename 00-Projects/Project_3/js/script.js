132b37a03cc9189e1f57373ed92018bac3dbc5bf
purplunicorn25 <40446921+purplunicorn25@users.noreply.github.com>
2020-04-02 18:03:50 -0400

    P3: JSON file finished; start screen item placed

--- 00-Projects/Project_3/js/script.js
+++ 00-Projects/Project_3/js/script.js
@@ -2,27 +2,58 @@
 
 /********************************************************************
 
-Title of Project
-Author Name
+Computational Wizard Duel
+Anne Boutet
 
-This is a template. Fill in the title, author, and this description
-to match your project! Write JavaScript to do amazing things below!
+DESCRIPTION!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 *********************************************************************/
 
 $(document).ready(setup);
 
+// Attacks, Blocks, and Items
+let spells;
+let counterSpells = [];
+let items = [];
 
+// Life of player and opponent in terms of battery level
+let playerPower;
+let opponentPower;
+
+//
+//
+//
 function setup() {
   $.getJSON("data/spells.json")
     .done(dataLoaded)
     .fail(dataError);
 }
 
+//
+//
+//
 function dataLoaded(data) {
-  console.log(loaded);
+  // Store the spells in an array
+  spells = data.spells;
+
+  // Display the draw to select who starts
+  $("#speed").selectmenu();
 }
 
+//
+//
+//
 function dataError(request, textStatus, error) {
   console.error(error);
+}
+
+// getRandomElement
+//
+// Get a random element from a specific array
+function getRandomElement(array) {
+  let element = array[Math.floor(Math.random() * array.length)];
+  return element;
 }
