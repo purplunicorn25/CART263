64bed27b6202e2c39891c929e5f15d913cc59012
purplunicorn25 <40446921+purplunicorn25@users.noreply.github.com>
2020-04-13 17:56:04 -0400

    P3: Start screen functionnal
    Decides on who starts first.

--- 00-Projects/Project_3/js/script.js
+++ 00-Projects/Project_3/js/script.js
@@ -27,9 +27,82 @@ let opponentPower;
 //
 //
 function setup() {
+  //
   $.getJSON("data/spells.json")
     .done(dataLoaded)
     .fail(dataError);
+  //
+  $("#draw").click(flicker);
+}
+
+//
+//
+//
+function flicker() {
+  //
+  $("#whiteBox").effect('pulsate', 'linear', 800, chooseFirstPlayer);
+  //
+  $("#draw").hide();
+}
+
+//
+//
+//
+function chooseFirstPlayer() {
+  let side;
+  //
+  $("#whiteBox").hide();
+  $("#blackBox").hide();
+  //
+  let randomNumber = Math.random();
+  //
+  if (randomNumber < 0.5) {
+    $("#whiteBox").show();
+    side = "white";
+  } else if (randomNumber > 0.5) {
+    $("#blackBox").show();
+    side = "black";
+  }
+  //
+  if ($("#side :selected").val() === side) {
+    //
+    $("#drawResult").text("YOU START!");
+    //
+    // player1round();
+    //
+    setTimeout(beginDuel, 1500);
+  } else {
+    //
+    $("#drawResult").text("YOUR OPPONENT STARTS...");
+    //
+    // opponent1round();
+    //
+    setTimeout(beginDuel, 1500);
+  }
+}
+
+//
+//
+//
+function beginDuel() {
+  //
+  $("#prepare").append("<h2>PREPARE TO DUEL</h2><p>Show respect to your opponent.</p>");
+  //
+  setTimeout(() => {
+    $(".bow").css("display", "inline-block");
+  }, 1500);
+  //
+  $(".bow").click(() => {
+    $("#ready").append("<p>Ready your keyboard...<br>");
+    //
+    setTimeout(() => {
+      $(".start").css("display", "inline-block");
+    }, 2000);
+  });
+  //
+  $(".start").click(() => {
+    $("#startMenu").hide();
+  })
 }
 
 //
@@ -50,6 +123,7 @@ function dataError(request, textStatus, error) {
   console.error(error);
 }
 
+/// ??????????????????????????????????????????????????????
 // getRandomElement
 //
 // Get a random element from a specific array
